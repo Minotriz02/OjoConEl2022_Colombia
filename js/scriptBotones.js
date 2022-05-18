ca318e09069f51ce2d6d01ed8a83636678cb9778
Juan Camilo Bolanos <juan_cam.bolanos@uao.edu.co>
2022-05-18 13:50:49 -0500

    modificaciones finales

--- js/scriptBotones.js
+++ js/scriptBotones.js
@@ -30,9 +30,9 @@ bt2.style.display = "none";
 contAfinidad.style.display = "none";
 contAfinidadFinal.style.display = "none";
 
-const P1 = new pregunta("video/1.mp4",'SI','NO',3,10,1);
-const P2 = new pregunta("video/FEP.mp4",'SI','NO',3,10,2);
-const P3 = new pregunta("video/SxF.mp4",'SI','NO',3,10,1);
+const P1 = new pregunta("video/1.mp4",'SI','NO',90,11,1);
+const P2 = new pregunta("video/2.mp4",'SI','NO',108,10,1);
+const P3 = new pregunta("video/3.mp4",'SI','NO',77,10,1);
 
 
 var preguntas =[P1,P2,P3];
@@ -40,6 +40,7 @@ var VA= 0;
 var esCorrecto=0;
 var numeroDeEstrellas =sessionStorage.getItem('Estrella');
 var numVideos= sessionStorage.getItem("numVideos");
+var afinidad;
 console.log(numVideos);
 
 bt1.addEventListener('click',cambiarVideo1,true);
@@ -48,7 +49,7 @@ btEmpe.addEventListener('click',reproducirVideo,true);
 btVolver.addEventListener('click',volver,true);
 
 definirVideo();
-
+calAfinidad();
 video.ontimeupdate= function(){    
     
     bt1.innerText = preguntas[VA].txt;
@@ -66,6 +67,12 @@ video.ontimeupdate= function(){
         bt2.style.display = "none";
     }
 };
+
+function calAfinidad() {
+ 
+    var porcNum = ((numeroDeEstrellas / 3) * 100).toFixed();
+    document.getElementById('afinV').innerHTML ="Afinidad: "+ porcNum + "%";
+}
 
 function definirVideo(){
     var vi = sessionStorage.getItem("Video");
@@ -123,11 +130,13 @@ function modificarBotones(){
     bt2.style.display = "none";
     numVideos++;
     sessionStorage.setItem("numVideos",numVideos);
-    var afinidad = ((numeroDeEstrellas/3)*100).toFixed();
+    afinidad = ((numeroDeEstrellas/3)*100).toFixed();
     var newTxt ="Tienes un un "+afinidad+"%"+" de compaginidad con Rodolfo Hernandez";
     console.log(newTxt);
     txtAfinidad.innerHTML = newTxt;
+    calAfinidad();
     if(numVideos>2){
+        linkRedes();
         contAfinidadFinal.style.display = "flex";
     }else{
         contAfinidad.style.display = "flex";
@@ -138,10 +147,12 @@ function modificarBotones(){
     }
     sessionStorage.setItem("numVideos",numVideos);
     video.pause();
+    insignias();
     ponerEstrella();
 }
 
 function ponerEstrella(){
+    console.log("aqui estoy" + numeroDeEstrellas)
     for(let i =0;i<estrellas.length;i++){
         if((i)<numeroDeEstrellas){
             estrellas[i].src="img/star2.png";
@@ -151,4 +162,27 @@ function ponerEstrella(){
     
 }
 
+function linkRedes(){
+    var btTwitter = document.getElementById("btTwitter");
+    btTwitter.href="https://twitter.com/intent/tweet?text=%c2%a1Tengo+un+"+afinidad+"%"+"25+de+afinidad+con+Rodolfo+Hern%c3%a1ndez!&hashtags=OjoConEl22";
+}
+
+function insignias(){
+    var txtIns = document.getElementById("txtNumeroEstrellas");
+    var newTxt ="Completaste una insignia de 3 posibles, vas por buen camino";
+    if(numeroDeEstrellas==1){
+        newTxt ="Completaste una insignia de 3 posible, vas por buen camino";
+    }
+
+    if(numeroDeEstrellas==2){
+        newTxt ="Completaste dos insignias de 3 posibles, vas por buen camino";
+    }
+
+    if(numeroDeEstrellas>2){
+        newTxt ="Completaste tres insignias de 3 posibles, vas por buen camino";
+    }
+  
+    console.log(newTxt);
+    txtAfinidad.innerHTML = newTxt;
+}
 
